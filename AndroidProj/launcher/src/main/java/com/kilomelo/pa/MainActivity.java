3d2f62db47b145cd8a1e7b61beb264b51e87ace9
Chenweichu <chenweichu@bytedance.com>
2023-02-01 21:29:15 +0800

    full screen unity view

--- AndroidProj/launcher/src/main/java/com/kilomelo/pa/MainActivity.java
+++ AndroidProj/launcher/src/main/java/com/kilomelo/pa/MainActivity.java
@@ -1,56 +1,124 @@
 package com.kilomelo.pa;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
+import android.widget.FrameLayout;
+import android.support.v7.app.AppCompatActivity;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import com.unity3d.player.MultiWindowSupport;
+import com.unity3d.player.UnityPlayer;
+import com.unity3d.player.UnityPlayerActivity;
 
-import com.kilomelo.pa.databinding.ActivityMainBinding;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    private FrameLayout fm;
+    private UnityPlayer mUnityPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i("MainActivity", "onCreate");
         setContentView(R.layout.activity_main);
-//        binding = ActivityMainBinding.inflate(getLayoutInflater());
-//        setContentView(binding.getRoot());
-//
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-//        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-//
-//        binding.fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+
+        fm = findViewById(R.id.fm);
+        mUnityPlayer = new UnityPlayer(this);
+        fm.addView(mUnityPlayer);
+//        UnityPlayer.UnitySendMessage("Car02_Door_FrontLeft", "getDoorLock", "1");
     }
 
-    @Override
-    public void onClick(View v) {
-        int viewId = v.getId();
-
-        Log.i("MainActivity", "onClick, viewId: " + viewId);
-
+    // Quit Unity
+    @Override protected void onDestroy ()
+    {
+        mUnityPlayer.destroy();
+        super.onDestroy();
     }
 
-//    @Override
-//    public boolean onSupportNavigateUp() {
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-//        return NavigationUI.navigateUp(navController, appBarConfiguration)
-//                || super.onSupportNavigateUp();
+    @Override protected void onStop()
+    {
+        super.onStop();
+
+        if (!MultiWindowSupport.getAllowResizableWindow(this))
+            return;
+
+        mUnityPlayer.pause();
+    }
+
+    @Override protected void onStart()
+    {
+        super.onStart();
+
+        if (!MultiWindowSupport.getAllowResizableWindow(this))
+            return;
+
+        mUnityPlayer.resume();
+    }
+
+    // Pause Unity
+    @Override protected void onPause()
+    {
+        super.onPause();
+
+        if (MultiWindowSupport.getAllowResizableWindow(this))
+            return;
+
+        mUnityPlayer.pause();
+    }
+
+    // Resume Unity
+    @Override protected void onResume()
+    {
+        super.onResume();
+
+        if (MultiWindowSupport.getAllowResizableWindow(this))
+            return;
+
+        mUnityPlayer.resume();
+    }
+
+    // Low Memory Unity
+    @Override public void onLowMemory()
+    {
+        super.onLowMemory();
+        mUnityPlayer.lowMemory();
+    }
+
+    // Trim Memory Unity
+    @Override public void onTrimMemory(int level)
+    {
+        super.onTrimMemory(level);
+        if (level == TRIM_MEMORY_RUNNING_CRITICAL)
+        {
+            mUnityPlayer.lowMemory();
+        }
+    }
+
+    // This ensures the layout will be correct.
+    @Override public void onConfigurationChanged(Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+        mUnityPlayer.configurationChanged(newConfig);
+    }
+
+    // Notify Unity of the focus change.
+    @Override public void onWindowFocusChanged(boolean hasFocus)
+    {
+        super.onWindowFocusChanged(hasFocus);
+        mUnityPlayer.windowFocusChanged(hasFocus);
+    }
+
+    // For some reason the multiple keyevent type is not supported by the ndk.
+    // Force event injection by overriding dispatchKeyEvent().
+//    @Override public boolean dispatchKeyEvent(KeyEvent event)
+//    {
+//        if (event.getAction() == KeyEvent.ACTION_MULTIPLE)
+//            return mUnityPlayer.injectEvent(event);
+//        return super.dispatchKeyEvent(event);
 //    }
+//
+//    // Pass any events not handled by (unfocused) views straight to UnityPlayer
+//    @Override public boolean onKeyUp(int keyCode, KeyEvent event)     { return mUnityPlayer.injectEvent(event); }
+//    @Override public boolean onKeyDown(int keyCode, KeyEvent event)   { return mUnityPlayer.injectEvent(event); }
+//    @Override public boolean onTouchEvent(MotionEvent event)          { return mUnityPlayer.injectEvent(event); }
+//    /*API12*/ public boolean onGenericMotionEvent(MotionEvent event)  { return mUnityPlayer.injectEvent(event); }
 }
