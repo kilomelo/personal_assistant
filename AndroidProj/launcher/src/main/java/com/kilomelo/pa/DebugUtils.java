f2693f8bbda0b140e356ba6b81df9901c99e8dec
Chenweichu <chenweichu@bytedance.com>
2023-02-03 14:18:40 +0800

    log switch

--- AndroidProj/launcher/src/main/java/com/kilomelo/pa/DebugUtils.java
+++ AndroidProj/launcher/src/main/java/com/kilomelo/pa/DebugUtils.java
@@ -3,8 +3,9 @@ package com.kilomelo.pa;
 import android.util.Log;
 
 public class DebugUtils {
+    // 打印函数调用日志开关
     public static boolean DEBUG_DEF_METHOD_LOG = true;
-    // 输出函数调用信息
+    // 打印函数调用日志
     public static void MethodLog() {
         if (!DEBUG_DEF_METHOD_LOG) return;
         StackTraceElement ste = new Throwable().getStackTrace()[1];
